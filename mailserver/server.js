da3b605b156bc7b4f8470db8f9c725ec76e99390
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2024-12-12 22:47:46 -0500

    v2.2

--- mailserver/server.js
+++ mailserver/server.js
@@ -63,7 +63,7 @@ async function startWebServer() {
 
 cron.schedule('0 2 * * *', async () => {
   console.log('Running a daily check for old emails and attachments...');
-  const oldEmails = await getOldEmails(30); // Get emails older than 30 days
+  const oldEmails = await getOldEmails(7); // Get emails older than 7 days
   console.log('oldEmails', oldEmails);
   for (const email of oldEmails) {
     await deleteEmailAndAttachments(email.emailID, email.emailId.toHexString()); // Delete each old email
