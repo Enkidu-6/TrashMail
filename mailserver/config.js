9b307ea9544b07e185019e72cb93b4a82c411977
Enkidu <101878139+Enkidu-6@users.noreply.github.com>
2024-12-14 12:48:19 -0500

    v2.2

--- mailserver/config.js
+++ mailserver/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 console.log("process.env.MONGO_URL", process.env.MONGO_URL);
 
 module.exports = {
-  smtpPort: 465,
+  smtpPort: 25,
   mongoURL: process.env.MONGO_URL || "mongodb://localhost:27017",
   dbName: "myemails",
   collectionName: "emails",
